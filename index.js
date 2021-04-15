9e621b6124ea309498d8eb311f1cdf8f7743756d
TheDevMinerTV <tobigames200@gmail.com>
2021-04-15 09:20:34 +0200

    Fixup the environment variable parsing

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const Express = require('express');
 const MailManager = require('./lib/MailManager');
 require('dotenv').config();
 
+if (
+	!(process.env.MAIL_SERVER && process.env.MAIL_SERVER_PORT && process.env.MAIL_USERNAME && process.env.MAIL_PASSWORD && process.env.PORT)
+) {
+	console.error('MAIL_SERVER, MAIL_SERVER_PORT, MAIL_USERNAME, MAIL_PASSWORD or PORT not set!');
+
+	process.exit(1);
+}
+
 let MAIL_SERVER_PORT = parseInt(process.env.MAIL_SERVER_PORT);
 let API_PORT = parseInt(process.env.PORT) || 22003;
 
 if (isNaN(MAIL_SERVER_PORT)) {
 	console.error('MAIL_SERVER_PORT is not a number!');
 
-	return;
+	process.exit(1);
 }
 
 if (isNaN(API_PORT)) {
 	console.error('API_PORT is not a number!');
 
-	return;
+	process.exit(1);
 }
 
 const transport = NodeMailer.createTransport({
